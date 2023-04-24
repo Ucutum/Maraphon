1372d9f10b18d1806091af4135719e0d57b5bcfe
Ucutum <t.musikov@yandex.ru>
2023-04-24 18:47:47 +0300

    finish

--- api_sample.py
+++ api_sample.py
@@ -2,12 +2,53 @@ import requests
 import json
 import datetime
 
-url = 'http://localhost:5000/api/task/0'
-headers = {'Content-Type': 'application/json'}
-data = {
-    "maraphone_id": 1, "name": "PostTask",
-    "date": datetime.date.today().strftime("%Y.%m.%d"),
-    "description": "---", 'api_key': 'key'}
-response = requests.post(url, headers=headers, data=json.dumps(data))
+KEY = "you key"
+URL = "http://localhost:5000"
 
-print(response.json())
+# castom errors:
+# 430 error - maraphone not found
+# 432 error - task not found
+
+
+def get_maraphone_by_id(maraphone_id):
+    url = URL + '/api/maraphone/' + str(maraphone_id)
+    headers = {'Content-Type': 'application/json'}
+    data = {'api_key': KEY}
+    response = requests.get(url, headers=headers, data=json.dumps(data))
+    # {'maraphone': {'created_date': '-', 'creator_id': -, 'id': -, 'title': '-'}}
+    return response.json()
+
+
+def get_task_list(maraphone_id):
+    url = URL + '/api/task_list/' + str(maraphone_id)
+    headers = {'Content-Type': 'application/json'}
+    data = {'api_key': KEY}
+    response = requests.get(url, headers=headers, data=json.dumps(data))
+    # {'tasks': [{'created_date': '-', 'date': '-', 'description': '-', 'id': -, 'main': {'creator_id': -}, 'main_id': -, 'name': '-'}]}
+    # main_id = id пользователя, который сделал
+    return response.json()
+
+
+def get_task(task_id):
+    url = URL + '/api/task/' + str(task_id)
+    headers = {'Content-Type': 'application/json'}
+    data = {'api_key': KEY}
+    response = requests.get(url, headers=headers, data=json.dumps(data))
+    # {'created_date': '-', 'date': '-', 'description': '-', 'id': -, 'main': {'creator_id': -}, 'main_id': -, 'name': '-'}
+    # main_id = id пользователя, который сделал
+    return response.json()
+
+
+def post_task(maraphone_id, name, date, description):
+    url = URL + '/api/task/0'
+    headers = {
+        'Content-Type': 'application/json'
+        }
+    data = {
+        "maraphone_id": maraphone_id, "name": name,
+        "date": date,
+        "description": description, 'api_key': KEY}
+    # date in format y.m.d
+    # you can use .strftime("%Y.%m.%d") from datetime
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, abort, redirect, url_for, flash, g, jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, abort, redirect, url_for, flash, jsonify
 import os
 import json
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
-from sqlalchemy.orm import Session
 import datetime
 import uuid
 
@@ -37,12 +35,12 @@ class MaraphoneApi(Resource):
     def get(self, id):
         args = api_key_parser.parse_args()
         if not check_api_key(args.api_key):
-            return abort(404, message="api key not found")
+            return jsonify({"error": 403, "message":"api key not found"})
 
         db = db_session.create_session()
         m = db.query(Maraphone).filter(Maraphone.id == id).first()
         if m is None:
-            return abort(404, message="maraphone not found")
+            return jsonify({"error": 430, "message":"maraphone not found"})
         return jsonify({"maraphone": m.to_dict()})
     
 
@@ -50,13 +48,13 @@ class TasksListApi(Resource):
     def get(self, maraphone_id):
         args = api_key_parser.parse_args()
         if not check_api_key(args.api_key):
-            return abort(404, message="api key not found")
+            return jsonify({"error": 403, "message":"api key not found"})
 
         db = db_session.create_session()
 
         m = db.query(Maraphone).filter(Maraphone.id == maraphone_id).first()
         if m is None:
-            return abort(404, message="maraphone not found")
+            return jsonify({"error": 430, "message":"maraphone not found"})
 
         t = db.query(Task).filter(Task.main_id == maraphone_id).all()
         return jsonify({"tasks": [i.to_dict() for i in t]})
@@ -80,11 +78,11 @@ class TasksApi(Resource):
         db = db_session.create_session()
         args = api_key_parser.parse_args()
         if not check_api_key(args.api_key):
-            return abort(404, message="api key not found")
+            return jsonify({"error": 403, "message":"api key not found"})
 
         t = db.query(Task).filter(Task.id == task_id).first()
         if t is None:
-            return abort(404, message="task not found")
+            return jsonify({"error": 432, "message":"task not found"})
         
         return jsonify(t.to_dict())
     
@@ -92,15 +90,17 @@ class TasksApi(Resource):
         args = parser.parse_args()
         db = db_session.create_session()
         if not check_api_key(args.api_key):
-            return abort(404, message="api key not found")
+            return jsonify({"error": 403, "message":"api key not found"})
 
         m = db.query(Maraphone).filter(Maraphone.id == args.maraphone_id).first()
         if m is None:
-            return abort(404, message="maraphone not found")
+            return jsonify({"error": 430, "message":"maraphone not found"})
         
-        u = db.query(User).filter(User.api_key == args.api_key)
+        u = db.query(User).filter(User.api_key == args.api_key).first()
+        if u is None:
+            return jsonify({"error": 403, "message":"not enough rights"})
         if m.creator_id != u.id:
-            return abort(403)
+            return jsonify({"error": 403, "message":"not enough rights"})
         
         time_str = args.date
         time_format = "%Y.%m.%d"
@@ -108,7 +108,7 @@ class TasksApi(Resource):
         try:
             time_obj = datetime.datetime.strptime(time_str, time_format)
         except ValueError:
-            return abort(400, "Error")
+            return jsonify({"error": 400, "message":"error with time"})
 
         t = Task(
             main_id=args.maraphone_id,
@@ -118,6 +118,7 @@ class TasksApi(Resource):
         
         db.add(t)
         db.commit()
+        return jsonify({"message":"ok"})
 
 api.add_resource(MaraphoneApi, "/api/maraphone/<int:id>")
 api.add_resource(TasksListApi, "/api/task_list/<int:maraphone_id>")
@@ -152,22 +153,6 @@ def make_header_data(url=None):
                 header["menu"][i] = (
                     header["menu"][i][0], header["menu"][i][1], False)
 
-# @app.before_request
-# def before_request():
-#     global db
-#     if not hasattr(g, "link_db"):
-#         g.link_db = db_session.create_session()
-#     db = g.link_db
-#     make_header_data()
-
-
-# @app.teardown_appcontext
-# def close_db(error):
-#     # if hasattr(g, 'link_db'):
-#     #     g.link_db.close()
-#     pass
-
-
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 
@@ -178,18 +163,63 @@ def load_user(user_id):
     return db.query(User).filter(User.id == user_id).first()
 
 
+@app.errorhandler(404)
+def error404(e):
+    return render_template("404error.html")
+
+
+@app.errorhandler(500)
+def error500(e):
+    return render_template("500error.html")
+
+
+@app.errorhandler(403)
+def error403(e):
+    return render_template("403error.html")
+
+
+@app.errorhandler(400)
+def error400(e):
+    return render_template("400error.html")
+
+
+def searching(db, q):
+    a = db.query(Maraphone).filter(Maraphone.title.contains(f"{ q }")).all()
+    return list(map(lambda x: (x.title, x.id), a))
+
+
 @app.route("/search", methods=["GET", "POST"])
 @login_required
 def search():
-    print("SEARCH|", request.form.get("search"))
-    return redirect(url_for("maraphon", id=request.form.get("search", "0")))
+    db = db_session.create_session()
+    mid = request.form.get("search", None)
+    if mid is None:
+        abort(404)
+    elif request.form.get("search").isdigit():
+        m = db.query(Maraphone).filter(Maraphone.id == mid).first()
+        if m is None:
+            return render_template(
+                "search_result.html", header=header,
+                maraphones=searching(db, mid),
+                question=mid)
+        else:
+            return redirect(url_for("maraphon", id=request.form.get("search", "0")))
+    else:
+        return render_template(
+            "search_result.html", header=header,
+            maraphones=searching(db, mid),
+            question=mid)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    db = db_session.create_session()
+    maraphones = list(map(
+        lambda x: (x.title, x.id),
+        (db.query(Maraphone).order_by(Maraphone.created_date).all()[::-1])[:10]))
     make_header_data(url_for("home"))
-    return render_template("home.html", header=header)
+    return render_template("home.html", header=header, maraphones=maraphones)
 
 
 @app.route("/create", methods=["GET", "POST"])
@@ -246,17 +276,14 @@ def edit(id):
             task.description = form.description.data
 
             if 'image' not in request.files:
-                print("no image")
+                pass
             else:
                 image = request.files["image"]
                 if not image.filename.strip():
-                    print("no image")
+                    pass
                 elif allowed_file(image.filename):
-                    print(image)
                     name = str(uuid.uuid4()) + image.filename[image.filename.rfind("."):]
-                    print(name)
                     image.save(os.path.join(app.config["UPLOAD_FOLDER"], name))
-                    print("ok")
                     task.image = name
                 else:
                     flash("Image is not validate.")
@@ -345,7 +372,6 @@ def account():
 @app.route("/logout")
 @login_required
 def logout():
-    print("logout")
     logout_user()
     return redirect(url_for("home"))
 
@@ -353,7 +379,6 @@ def logout():
 @app.route("/remake_api")
 @login_required
 def remake_api():
-    print("remake_api")
     db = db_session.create_session()
     u = db.query(User).filter(User.id == current_user.id).first()
     u.api_key = str(uuid.uuid4())
@@ -506,7 +531,6 @@ def state(id):
     if not state:
         return json.dumps({"responsible": False, "error": "has not state"})
 
-    print(state.task.date, datetime.datetime.today().date())
     if state.task.date != datetime.datetime.today().date():
         return json.dumps(
             {"responsible": False, "state": state.done, "error": "date error"})
@@ -545,4 +569,5 @@ def maraphon_settings(id):
 
 
 if __name__ == "__main__":
+    # app.run(host="0.0.0.0")
     app.run(debug=True)
